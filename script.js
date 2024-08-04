81e46d099b8a1c5eee4e86f15a93e390e84879c1
Thomaz <79727201+thomazgg@users.noreply.github.com>
2024-08-04 00:45:52 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml12");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+  targets: ".ml12 .letter",
+  translateY: [100, 0],
+  translateZ: 0,
+  opacity: [0, 1],
+  easing: "easeOutExpo",
+  duration: 2000,
+  delay: (el, i) => 2000 + 60 * i,
+});
+
+TweenMax.to(".btn-shine", 3, {
+  opacity: 0,
+  ease: Expo.easeInOut,
+  delay: 0.5,
+});
+
+TweenMax.from(".left", 3, {
+  left: "-50%",
+  ease: Expo.easeInOut,
+  delay: 3.4,
+});
+
+TweenMax.from(".header h1", 3, {
+  left: "-100%",
+  ease: Expo.easeInOut,
+  delay: 3.4,
+});
+
+TweenMax.staggerFrom(
+  ".images > div",
+  1,
+  {
+    y: "60",
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 6,
+  },
+  0.2
+);
+
+TweenMax.staggerFrom(
+  ".main-content > .anim",
+  1,
+  {
+    y: "60",
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 5.6,
+  },
+  0.2
+);
